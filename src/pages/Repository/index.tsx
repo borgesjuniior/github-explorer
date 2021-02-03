d5c8073aabeb75fcbfaecb074d3dd1555805fb05
Júnior <valdecyborgesjr@hotmail.com>
2021-02-03 20:44:04 -0300

    criando rotas

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Repositoruy: React.FC = () => {
+  return <h1>Repository</h1>
+}
+
+export default Repositoruy;
---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  /**
+   * O Switch garante que apenas uma rota seja
+   * exibida
+   */
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/Repository" component={Repository} />
+  </Switch>
+)
+
+export default Routes;
